a08d6a5dfed33046e76d4f8a335e06ab846ebbb3
Ryan Carr <rycarr@gmail.com>
2019-05-07 14:57:43 -0500

    Added additional properties to RouteModel::Node including a method to find euclidean distance between two nodes

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -1,6 +1,13 @@
 #include "route_model.h"
 #include <iostream>
 
-RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
+RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) 
+{
+	int counter = 0;
 
+	for(Model::Node node : this->Nodes())
+	{
+		m_Nodes.push_back(RouteModel::Node(counter, this, node));
+		counter++;
+	}
 }
--- src/route_model.h
+++ src/route_model.h
@@ -11,7 +11,13 @@ class RouteModel : public Model {
   public:
     class Node : public Model::Node {
       public:
-        // Add public Node variables and methods here.
+        float g_value = 0.f;
+        float h_value = std::numeric_limits<float>::max();
+        bool visited = false;
+        Node *parent = nullptr;
+        std::vector<Node *> neighbors;
+
+        float distance(Node dest) const { return std::sqrt( std::pow(this->x - dest.x, 2)+ std::pow(this->y - dest.y, 2) ); }
         
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
@@ -23,10 +29,11 @@ class RouteModel : public Model {
     };
     
     // Add public RouteModel variables and methods here.
-    RouteModel(const std::vector<std::byte> &xml);  
+    RouteModel(const std::vector<std::byte> &xml);
+    auto &SNodes() { return m_Nodes; };
     std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
 
   private:
-    // Add private RouteModel variables and methods here.
+    std::vector<Node> m_Nodes;
 
 };
